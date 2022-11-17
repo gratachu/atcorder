181d64a0e5305311cb9e5af70acce45c866612e2
gratachu <gratachu9@gmail.com>
2022-11-17 22:52:15 +0900

    Add we_love_golf

---
+++ day4/we_love_golf.rb
@@ -0,0 +1,13 @@
+base = gets.chomp.to_i
+range_arr = gets.chomp.split(" ").map(&:to_i)
+
+result = "NG"
+
+for i in range_arr[0]..range_arr[1] do
+  if i % base == 0
+    result = "OK"
+    break
+  end
+end
+
+print(result)
